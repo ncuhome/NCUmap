439a275a3f0ccb7d9b725a7f9bcc78db8292fdf6
WOo0W <24245520+WOo0W@users.noreply.github.com>
2022-07-02 20:26:19 +0800

    feat: add script for generating Map.tsx

---
+++ scripts/generate_model.py
@@ -0,0 +1,40 @@
+import re 
+
+with open('../src/Map.tsx', 'r') as f:
+    s = f.read()
+
+MATCH_JSX = re.compile(r'<mesh(.*?)/>', re.DOTALL)
+
+MATCH_CN = re.compile(r'[\u4e00-\u9fff]+')
+
+MATCH_NAME = re.compile(r'geometry={nodes\.(.+?)\.geometry}')
+
+matches: list[str] = MATCH_JSX.findall(s)
+
+# Add labels for all mesh with Chinese name
+for match in matches:
+    lines = match.splitlines()
+    print(lines, '\n')
+
+    name = MATCH_NAME.search(match)
+    if name:
+        name = name.group(1)
+        if MATCH_CN.search(name):
+            lines.insert(
+                -1, f'''
+            children={{<Html>{name}</Html>}}
+            ''')
+            s = s.replace(match, '\n'.join(lines), 1)
+
+lines = s.splitlines()
+
+# Add import for gltf file
+lines.insert(0, '''import modelUrl from "/map.glb?url";''')
+lines = [line.replace('"/map.glb"', 'modelUrl') for line in lines]
+
+# Fix ref error
+s = '\n'.join(lines)
+s = s.replace('const group = useRef<THREE.Group>();',
+              'const group = useRef<THREE.Group>(null);', 1)
+with open('../src/Map.tsx', 'w') as f:
+    f.write(s)
