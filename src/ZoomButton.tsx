ceb517597cff07f106c200f560545fbb89763b0b
sunnywind <sunny-wind@qq.com>
2022-07-05 14:05:22 +0800

    :zap: animate ZoomButton

--- src/ZoomButton.tsx
+++ src/ZoomButton.tsx
@@ -1,22 +1,29 @@
-import { useBounds } from "@react-three/drei"
-import { useTrackedStore } from "./store"
+import { useTrackedStore } from "./store";
 
-export default function ZoomButton(){
-    const {isZoomed,setZoomed} = useTrackedStore()
-    const api = useBounds()
-    const handleBack = ()=>{
-        api.refresh().fit()
-    }
-    return(
-        <div style={{
-            position:'absolute',
-            bottom: 10,
-            left:'48%',
-            display:isZoomed?'block':'none'
-        }}>
-            <button onClick={()=>(setZoomed(false))}>
-                OverView 
-            </button>
-        </div>
-    )
+export default function ZoomButton() {
+  const { isZoomed, setZoomed } = useTrackedStore();
+
+  return (
+    <div
+      style={{
+        position: "absolute",
+        bottom: 10,
+        left: "48%",
+        transform: isZoomed ? "" : "translateY(50px)",
+        transition: "all 0.5s ease-in-out",
+      }}
+    >
+      <button
+        onClick={() => setZoomed(false)}
+        style={{
+          backgroundColor: "transparent",
+          borderRadius: "5px",
+          transform: " scale(1.5)",
+          cursor: "pointer",
+        }}
+      >
+        OverView
+      </button>
+    </div>
+  );
 }
