4877961ad465d58a0d028d429c03a54dacdffe83
WOo0W <24245520+WOo0W@users.noreply.github.com>
2022-07-06 14:09:03 +0800

    fix #1 with pyftsubset from python

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { CJK, literalsCollector } from "vite-plugin-literals-collector";
+import { execSync } from "child_process";
+import { readdirSync, renameSync } from "fs";
+
+const minifyFont = (target: string[]) => {
+  if (target.length > 0) {
+    const text = target.join("").replace(" ", "");
+    console.log(text, readdirSync("dist/assets"));
+
+    const file =
+      "dist/assets/" +
+      readdirSync("dist/assets").filter((f) => f.endsWith("otf"))[0];
+    execSync(`pyftsubset ${file} --text=${text}`);
+    renameSync(file.replace(".otf", ".subset.otf"), file);
+  }
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    literalsCollector({
+      include: [/Map\.tsx$/],
+      target: CJK,
+      onResult: minifyFont,
+    }),
+  ],
+});
