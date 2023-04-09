84dd6abcb98e48ac01e223506f2099e91ae0e3e6
ComfyFluffy <24245520+ComfyFluffy@users.noreply.github.com>
2023-04-09 13:56:39 +0000

    move back botton to top-left

--- src/components/Back.tsx
+++ src/components/Back.tsx
@@ -1,9 +1,8 @@
-
 export default function Back({ onClick = () => {} }) {
   return (
     <div
       onClick={onClick}
-      className="px-6 py-1 absolute bottom-2 left-2 text-center bg-white rounded-full shadow-2xl opacity-90 pointer-events-auto"
+      className="px-6 py-1 absolute top-2 left-2 text-center bg-white rounded-full shadow-2xl opacity-90 pointer-events-auto"
     >
       返回
     </div>
